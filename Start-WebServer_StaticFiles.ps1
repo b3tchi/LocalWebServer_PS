9dde90b78f178cf1caae9d9b5270030f1b9243c9
Jan Becka <beckaja1@gmail.com>
2020-05-03 22:26:58 +0200

    added server items

---
+++ Start-WebServer_StaticFiles.ps1
@@ -0,0 +1,30 @@
+﻿$Hso = New-Object Net.HttpListener
+$Hso.Prefixes.Add("http://localhost:8001/")
+$Hso.Start()
+# Register-EngineEvent PowerShell.Exiting –Action { 
+#     Write-Host "Close Event"
+#  }
+try{
+	"$(Get-Date -Format s) Custom  Powershell webserver started."
+    While ($Hso.IsListening) {
+        $HC = $Hso.GetContext()
+        $HRes = $HC.Response
+        # Write-Output $HC.Request
+        # $HRes.Headers.Add("Content-Type","text/plain")
+        $Request = (Join-Path $Pwd ($HC.Request).RawUrl)
+        if($Request -like "*.css"){
+            Write-Host "Css"
+            $HRes.Headers.Add("Content-Type","text/css")
+        }
+        Write-Host $Request  
+        $Buf = [Text.Encoding]::UTF8.GetBytes((Get-Content (Join-Path $Pwd ($HC.Request).RawUrl)-Raw))
+        # $Buf =Get-Content(Join-Path $Pwd ($HC.Request).RawUrl) -Raw
+        $HRes.ContentLength64 = $Buf.Length
+        $HRes.OutputStream.Write($Buf,0,$Buf.Length)
+        $HRes.Close()
+    }
+}finally{
+    Write-Host "Closed"
+    $Hso.Stop()
+    $Hso.Close()
+}
